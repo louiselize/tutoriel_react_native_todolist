cdff496d56b51d149187292e4eacd2f963cfd7bf
Louise Lizé <louise.lize@icloud.com>
2025-10-28 16:42:36 +0100

    Add layout structure and styles for App component

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
-import { Text } from "react-native";
+import { Text, View } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import { styles } from "./App.style";
+
 export default function App() {
   return (
-    <SafeAreaProvider>
-      <SafeAreaView>
-        <Text>Hello, World!</Text>
-      </SafeAreaView>
-    </SafeAreaProvider>
+    <>
+      <SafeAreaProvider>
+        <SafeAreaView style={styles.app}>
+          <View style={styles.header}>
+            <Text>Header</Text>
+          </View>
+          <View style={styles.body}>
+            <Text>Body</Text>
+          </View>
+        </SafeAreaView>
+      </SafeAreaProvider>
+      <View style={styles.footer}>
+        <Text>Footer</Text>
+      </View>
+    </>
   );
 }
---
+++ App.style.js
@@ -0,0 +1,17 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  app: {
+    backgroundColor: "#F9F9F9",
+    flex: 1,
+  },
+  header: {
+    flex: 1,
+  },
+  body: {
+    flex: 5,
+  },
+  footer: {
+    height: 70,
+  },
+});
